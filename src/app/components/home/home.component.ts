a2303c9900240c12727cc43b4c43efa065096820
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-07-13 12:12:36 +0200

    Minor updates to data and styles
    Deleted unused stuff.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import { ConnectionService } from 'src/app/services/connection.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  displayedColumns: string[] = ['targa', 'concessionario', 'marchio', 'maxSpeed', 'cilindrata', 'edit', 'delete'];
+  displayedColumns: string[] = ['Id', 'Name', 'Brand', 'Speed', 'MaxSpeed', 'Weight', 'Displacement', 'edit', 'delete'];
   dataSource: any;
   car: ICar[] = [];
 
@@ -26,26 +26,3 @@ export class HomeComponent implements OnInit{
   }
 
 }
-
-// export interface Cars {
-//   targa: string;
-//   concessionario: string;
-//   marchio: string;
-//   maxSpeed: number;
-//   cilindrata: number;
-// }
-
-// const ELEMENT_DATA: Cars[] = [
-//   {targa: "AA000AA", concessionario: 'Hydrogen', marchio: "Mercedes", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA001AA", concessionario: 'Helium', marchio: "Ferrari", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA002AA", concessionario: 'Lithium', marchio: "Fiat", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA003AA", concessionario: 'Beryllium', marchio: "Lamborghini", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA004AA", concessionario: 'Boron', marchio: "BMW", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA005AA", concessionario: 'Carbon', marchio: "Opel", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA006AA", concessionario: 'Nitrogen', marchio: "Alfa", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA007AA", concessionario: 'Oxygen', marchio: "Honda", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA008AA", concessionario: 'Fluorine', marchio: "Fiat", maxSpeed: 350, cilindrata: 250},
-//   {targa: "AA009AA", concessionario: 'Neon', marchio: "Maserati", maxSpeed: 350, cilindrata: 250},
-// ];
-
-
