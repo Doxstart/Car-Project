f39db489c742b867cfaae4a03bdf2026e4124f84
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-07-24 14:21:00 +0200

    Updated home component TS

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { ICarDealer } from 'src/app/models/car.models';
+import { ICarDealer, ICars } from 'src/app/models/car.models';
 import { ConnectionService } from 'src/app/services/connection.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
   displayedColumns: string[] = [
-    'DealerId',
-    'DealerName',
-    'CarId',
-    'Plate',
-    'Name',
-    'Brand',
-    'Speed',
-    'MaxSpeed',
-    'Weight',
-    'Displacement',
-    'edit',
-    'delete',
+    'dealerId',
+    'dealerName',
   ];
-  dataSource: any;
+  secondaryColumns: string[] = [
+    'id',
+    'plate',
+    'name',
+    'brand',
+    'speed',
+    'maxSpeed',
+    'weight',
+    'displacement'
+  ];
+  secondaryDisplayColumns = [...this.secondaryColumns, 'delete', 'edit'];
+  expandedElement!: ICarDealer | null;
+  dataSource!: any;
   car: ICarDealer[] = [];
 
-  constructor(private readonly connServ: ConnectionService) {}
+  constructor(private readonly connServ: ConnectionService, private cd: ChangeDetectorRef) {}
   ngOnInit(): void {
     this.connServ.getCars().subscribe({
       next: (data: ICarDealer[]) => (
@@ -37,4 +47,14 @@ export class HomeComponent implements OnInit {
       error: (err: any) => console.log(err),
     });
   }
+
+  toggleRow(element: ICarDealer) {
+    element.listofCars && element.listofCars.length ? (this.expandedElement = this.expandedElement === element ? null : element) : null;
+    this.cd.detectChanges();
+  }
+
+  onDelete(car: ICars) {
+    console.log('Clicked car', car);
+  }
+
 }
